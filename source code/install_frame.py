c9aa2352f4465faad93e2f69b7f5b78cc6778b82
jooyoung.han <jooyoung.han2.mil@army.mil>
2024-04-14 14:52:36 -0700

    fixing postgresql installation

--- source code/install_frame.py
+++ source code/install_frame.py
@@ -50,12 +50,12 @@ def install_tak():
         if('postgresql.asc' not in check_keyrings_dir or 'postgresql-15' not in check_postgresql[1]):
             install_postgres1 = 'sudo curl https://www.postgresql.org/media/keys/ACCC4CF8.asc --output /etc/apt/keyrings/postgresql.asc'
             install_postgres2 = "'"'echo "deb [signed-by=/etc/apt/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" /etc/apt/sources.list.d/postgresql.list'"'"
+            subprocess.run(['sudo', 'apt', 'update'])
+            subprocess.run(['sudo', 'apt', 'upgrade'])
             subprocess.run(install_postgres1.split(' '))
             test = subprocess.run(['sudo', 'sh', '-c', install_postgres2])
             print(' '.join(test.args))
-            subprocess.run(['sudo', 'apt', 'update'])
             subprocess.run(['sudo', 'apt', 'install', 'postgresql-15'])
-            # subprocess.run(['sudo', 'apt', 'upgrade'], input=b'y\n')
             if('postgresql-15' in check_postgresql[1]):
                 print('\n////////// postgresql installed. //////////')
         else:
