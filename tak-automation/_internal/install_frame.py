c2fe3219c71b6404ec8392434de2b83304282014
jooyoung.han <jooyoung.han2.mil@army.mil>
2024-04-14 15:17:18 -0700

    fixing postgresql installation

--- tak-automation/_internal/install_frame.py
+++ tak-automation/_internal/install_frame.py
@@ -49,12 +49,11 @@ def install_tak():
         check_postgresql = subprocess.run(['sudo', 'apt', 'list', '--installed', 'postgresql-15'], stdout=subprocess.PIPE, universal_newlines=True).stdout.split('\n')
         if('postgresql.asc' not in check_keyrings_dir or 'postgresql-15' not in check_postgresql[1]):
             install_postgres1 = 'sudo curl https://www.postgresql.org/media/keys/ACCC4CF8.asc --output /etc/apt/keyrings/postgresql.asc'
-            install_postgres2 = "'"'echo "deb [signed-by=/etc/apt/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/postgresql.list'"'"
-            subprocess.run(['sudo', 'apt', 'update'])
+            install_postgres2 = "'"'echo "deb [signed-by=/etc/apt/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/postgresql.list'"'"
             subprocess.run(install_postgres1.split(' '))
-            test = subprocess.run(['sudo', 'sh', '-c', install_postgres2])
-            print(' '.join(test.args))
-            subprocess.run(['sudo', 'apt', 'install', 'postgresql'])
+            subprocess.run(['sudo', 'sh', '-c', install_postgres2])
+            subprocess.run(['sudo', 'apt', 'update'])
+            subprocess.run(['sudo', 'apt', 'install', 'postgresql-15'])
             if('postgresql-15' in check_postgresql[1]):
                 print('\n////////// postgresql installed. //////////')
         else:
